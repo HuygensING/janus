1a346b80bc7426736ef8bcbf9effe071759b18c2
LB <lhuygi@users.noreply.github.com>
2017-07-20 11:32:06 +0200

    Improve XML reconstruction test

--- src/test/java/nl/knaw/huygens/pergamon/janus/xml/TestTaggedCodepoints.java
+++ src/test/java/nl/knaw/huygens/pergamon/janus/xml/TestTaggedCodepoints.java
@@ -19,7 +19,11 @@ public class TestTaggedCodepoints extends TestTaggedText {
   private static final String HEADER = "<?xml version=\"1.0\"?>\n";
 
   private void test(String expect) {
-    String got = ((TaggedCodepoints) parse(expect)).reconstruct().toXML();
+    test(expect, expect);
+  }
+
+  private void test(String input, String expect) {
+    String got = ((TaggedCodepoints) parse(input)).reconstruct().toXML();
     assertEquals(HEADER + expect + "\n", got);
   }
 
@@ -28,7 +32,10 @@ public class TestTaggedCodepoints extends TestTaggedText {
     test("<x><p><q /></p></x>");
     test("<x><p /><q /></x>");
     test("<p> hello <q>xml <r /> and json</q> world</p>");
-    test("<p at=\"what?\" />");
     test("<tricky>\uD834\uDD20<a />\uD834\uDD20 got through! </tricky>");
+
+    // Whitespace will be normalized, attributes sorted.
+    test("<p   x=\"who?\" b=\"bla\"  z=\"foo\"  a=\"what?\"/>",
+      "<p a=\"what?\" b=\"bla\" x=\"who?\" z=\"foo\" />");
   }
 }
