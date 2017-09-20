59e310b520732ad5fb964584cc057fd99c98fd15
HDJ <hayco.de.jong@huygens.knaw.nl>
2017-09-20 11:36:47 +0200

    Add request and response headers to structured log

--- src/main/java/nl/knaw/huygens/pergamon/janus/logging/RequestLoggingFilter.java
+++ src/main/java/nl/knaw/huygens/pergamon/janus/logging/RequestLoggingFilter.java
@@ -11,22 +11,38 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @PreMatching
 @Priority(Integer.MIN_VALUE)
 public class RequestLoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
-  private static final String MDC_ID = "id";
+  public static final Comparator<Map.Entry<String, List<String>>> BY_KEY_IGNORING_CASE =
+    (e1, e2) -> e1.getKey().compareToIgnoreCase(e2.getKey());
 
   private static final String STOPWATCH_PROPERTY = RequestLoggingFilter.class.getName() + "stopwatch";
 
   private static final Logger LOG = LoggerFactory.getLogger(RequestLoggingFilter.class);
 
+  private static final String MDC_ID = "id";
+  private static final String MDC_REQUEST_METHOD = "http_method";
+  private static final String MDC_REQUEST_URI = "request_uri";
+  private static final String MDC_REQUEST_HEADERS = "request_headers";
+
   @Override
   public void filter(ContainerRequestContext context) throws IOException {
+    MDC.put("type", "request");
     MDC.put(MDC_ID, UUID.randomUUID().toString());
+    MDC.put(MDC_REQUEST_METHOD, context.getMethod());
+    MDC.put(MDC_REQUEST_URI, context.getUriInfo().getRequestUri().toASCIIString());
+    MDC.put(MDC_REQUEST_HEADERS, formatHeaders(context.getHeaders()));
     LOG.info(">     " + context.getMethod() + " " + context.getUriInfo().getRequestUri().toASCIIString());
     context.setProperty(STOPWATCH_PROPERTY, Stopwatch.createStarted());
   }
@@ -34,6 +50,7 @@ public class RequestLoggingFilter implements ContainerRequestFilter, ContainerRe
   @Override
   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
     throws IOException {
+    MDC.put("type", "response");
     String msg = "< " + responseContext.getStatus() + " " + requestContext.getMethod() + " " +
       requestContext.getUriInfo().getRequestUri().toASCIIString();
 
@@ -42,10 +59,29 @@ public class RequestLoggingFilter implements ContainerRequestFilter, ContainerRe
       LOG.warn("Lost my stopwatch!");
     } else if (!stopwatch.isRunning()) {
       LOG.warn("Stopwatch was stopped!");
-    }
-    else {
+    } else {
+      MDC.put("elapsed_ms", String.valueOf(stopwatch.elapsed(TimeUnit.MILLISECONDS)));
       msg += String.format(" (%d ms)", stopwatch.elapsed(TimeUnit.MILLISECONDS));
     }
+
+    MDC.put("response_headers", formatHeaders(responseContext.getStringHeaders()));
     LOG.debug(msg);
   }
+
+  private String formatHeaders(final MultivaluedMap<String, String> headers) {
+    final StringBuilder builder = new StringBuilder();
+    sortHeaders(headers.entrySet())
+      .forEach(entry -> builder.append(entry.getKey())
+                               .append(": ")
+                               .append(String.join(",", entry.getValue()))
+                               .append('\n'));
+    return builder.toString();
+  }
+
+  private Set<Map.Entry<String, List<String>>> sortHeaders(final Set<Map.Entry<String, List<String>>> headers) {
+    final TreeSet<Map.Entry<String, List<String>>> sorted = new TreeSet<>(BY_KEY_IGNORING_CASE);
+    sorted.addAll(headers);
+    return sorted;
+  }
+
 }
