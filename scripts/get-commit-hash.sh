d6efb389d94d45bbcf1cbb4b900c02edbe5e3db3
HDJ <hayco.de.jong@huygens.knaw.nl>
2017-09-27 13:46:41 +0200

    Remove bogus git.commit.time

--- scripts/get-commit-hash.sh
+++ scripts/get-commit-hash.sh
@@ -3,7 +3,6 @@
 mkdir -p target/classes
 cat > target/classes/git.properties <<ENDCAT
 git.branch=${SOURCE_BRANCH:-"BRANCH_NOT_SET"}
-git.commit.time=$(date)
 git.commit.id=${SOURCE_COMMIT:-"COMMIT_HASH_NOT_SET"}
 git.commit.msg=${COMMIT_MSG:-"COMMIT_MSG_NOT_SET"}
 docker.tag=${CACHE_TAG:-"CACHE_TAG_NOT_SET"}
