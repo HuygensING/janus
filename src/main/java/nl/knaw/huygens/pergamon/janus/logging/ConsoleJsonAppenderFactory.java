90a748e3e32cd88ec3394840deea13e0cf1c39dc
LB <lhuygi@users.noreply.github.com>
2017-10-06 13:57:25 +0200

    Further simplify logging stuff

--- src/main/java/nl/knaw/huygens/pergamon/janus/logging/ConsoleJsonAppenderFactory.java
+++ src/main/java/nl/knaw/huygens/pergamon/janus/logging/ConsoleJsonAppenderFactory.java
@@ -6,13 +6,17 @@ import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.ConsoleAppender;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.logging.AbstractAppenderFactory;
 import io.dropwizard.logging.async.AsyncAppenderFactory;
 import io.dropwizard.logging.filter.LevelFilterFactory;
 import io.dropwizard.logging.layout.LayoutFactory;
 import net.logstash.logback.encoder.LogstashEncoder;
+import net.logstash.logback.fieldnames.LogstashFieldNames;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @JsonTypeName("console-json")
 public class ConsoleJsonAppenderFactory extends AbstractAppenderFactory<ILoggingEvent> {
@@ -55,7 +59,7 @@ public class ConsoleJsonAppenderFactory extends AbstractAppenderFactory<ILogging
     //   customFields:
     //     "appName": "Janus"
     if (customFields != null) {
-      AppenderFactoryHelper.getCustomFieldsFromHashMap(customFields).ifPresent(encoder::setCustomFields);
+      getCustomFieldsFromHashMap(customFields).ifPresent(encoder::setCustomFields);
     }
 
     // allow overriding default field names, e.g.,
@@ -63,7 +67,7 @@ public class ConsoleJsonAppenderFactory extends AbstractAppenderFactory<ILogging
     //     "message": "msg"
     //     "timestamp": "@time"
     if (fieldNames != null) {
-      encoder.setFieldNames(AppenderFactoryHelper.getFieldNamesFromHashMap(fieldNames));
+      encoder.setFieldNames(getFieldNamesFromHashMap(fieldNames));
     }
 
     // setup encoder
@@ -80,4 +84,35 @@ public class ConsoleJsonAppenderFactory extends AbstractAppenderFactory<ILogging
     return wrapAsync(appender, asyncAppenderFactory);
   }
 
+  private static Optional<String> getCustomFieldsFromHashMap(HashMap<String, String> map) {
+    try {
+      return Optional.of(new ObjectMapper().writeValueAsString(map));
+    } catch (JsonProcessingException e) {
+      System.err.println("Unable to parse customFields: " + e.getMessage());
+      return Optional.empty();
+    }
+  }
+
+  private static LogstashFieldNames getFieldNamesFromHashMap(HashMap<String, String> map) {
+    LogstashFieldNames fieldNames = new LogstashFieldNames();
+
+    fieldNames.setTimestamp(map.getOrDefault("timestamp", "@timestamp"));
+    fieldNames.setVersion(map.getOrDefault("version", "@version"));
+    fieldNames.setMessage(map.getOrDefault("message", "message"));
+    fieldNames.setLogger(map.getOrDefault("logger", "logger_name"));
+    fieldNames.setThread(map.getOrDefault("thread", "thread_name"));
+    fieldNames.setLevel(map.getOrDefault("level", "level"));
+    fieldNames.setLevelValue(map.getOrDefault("levelValue", "level_value"));
+    fieldNames.setCaller(map.get("caller"));
+    fieldNames.setCallerClass(map.getOrDefault("callerClass", "caller_class_name"));
+    fieldNames.setCallerMethod(map.getOrDefault("callerMethod", "caller_method_name"));
+    fieldNames.setCallerFile(map.getOrDefault("callerFile", "caller_file_name"));
+    fieldNames.setCallerLine(map.getOrDefault("callerLine", "caller_line_number"));
+    fieldNames.setStackTrace(map.getOrDefault("stackTrace", "stack_trace"));
+    fieldNames.setTags(map.getOrDefault("tags", "tags"));
+    fieldNames.setMdc(map.get("mdc"));
+    fieldNames.setContext(map.get("context"));
+
+    return fieldNames;
+  }
 }
