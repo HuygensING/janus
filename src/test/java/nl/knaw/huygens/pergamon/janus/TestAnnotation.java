41c3fd2309274c92792f9895ab6199b2458b906d
HDJ <hayco.de.jong@huygens.knaw.nl>
2017-10-11 10:31:36 +0200

    Remove explicit dependency on jackson-annotations

--- src/test/java/nl/knaw/huygens/pergamon/janus/TestAnnotation.java
+++ src/test/java/nl/knaw/huygens/pergamon/janus/TestAnnotation.java
@@ -1,7 +1,6 @@
 package nl.knaw.huygens.pergamon.janus;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.dropwizard.jackson.Jackson;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,7 +10,7 @@ import static org.junit.Assert.assertFalse;
 public class TestAnnotation {
   @Test
   public void emptyBody() throws IOException {
-    ObjectMapper mapper = Jackson.newObjectMapper();
+    ObjectMapper mapper = new ObjectMapper();
     Annotation ann = mapper.readValue("{\"start\": 1, \"end\": 2}", Annotation.class);
     String s = mapper.writeValueAsString(ann);
     assertFalse(s.contains("body"));
